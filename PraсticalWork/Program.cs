214f778e4c4e4dfc4a0c61745515da40e8e94781
Vlad Ivanko <vladivanko@gmail.com>
2025-05-16 15:52:52 +0300

    Add age and nickname

--- PraсticalWork/Program.cs
+++ PraсticalWork/Program.cs
@@ -1,9 +1,58 @@
+using System;
+
 namespace PraсticalWork
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            Console.WriteLine("Hello, Welcome to the game: Stone Scissors Paper");
+            Console.Write("Enter your Nickname:");
+            string nickname = Console.ReadLine();
+            {
+                while (string.IsNullOrWhiteSpace(nickname))
+                {
+                    Console.Write("Nickname cannot be empty.\nEnter your Nickname:");
+                    nickname = Console.ReadLine();
+                }
+            }
+            int age = 0;
+            bool isAgeInt = false;
+                  while (!isAgeInt)
+                  {
+                      Console.Write("Enter your age:");
+                       string ageInt = Console.ReadLine();
+                       if (string.IsNullOrWhiteSpace(ageInt))
+                       {
+                           Console.WriteLine("Age cannot be empty. Try again.");
+                           continue;
+                       }
+                       bool isNumber = int.TryParse(ageInt, out age);
+                       if (!isNumber)
+                       {
+                           Console.WriteLine("Age must be a number. Try again.");
+                           continue;
+                       }
+
+                       if (age < 12)
+                       {
+                           Console.WriteLine("Sorry, the game is only available from 12 years old.");
+                           continue;
+                       }
+                       isAgeInt = true;
+                  }
+                  Console.WriteLine($"Your age is {age}. Let's continue!");
+            
+            
+              
+                  
+            
+                
+              
+               
+            
+            
+            
         }
     }
 }
