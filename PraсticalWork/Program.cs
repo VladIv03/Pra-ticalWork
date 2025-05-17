2fc9394a551cfeccc79c74666974e76b50f2eb5f
Vlad Ivanko <vladivanko@gmail.com>
2025-05-17 17:36:04 +0300

    add battle

--- PraсticalWork/Program.cs
+++ PraсticalWork/Program.cs
@@ -27,52 +27,101 @@ namespace PraсticalWork
                     Console.WriteLine("Age cannot be empty. Try again.");
                     continue;
                 }
-
                 var isNumber = int.TryParse(ageInt, out age);
                 if (!isNumber)
                 {
                     Console.WriteLine("Age must be a number. Try again.");
                     continue;
                 }
-
                 if (age < 12)
                 {
                     Console.WriteLine("Sorry, the game is only available from 12 years old.");
                     continue;
                 }
-
                 isAgeInt = true;
             }
-
+            int playerTotalWins = 0;
             Console.WriteLine($"Your age is {age}. Let's continue!");
-            Console.WriteLine("========================================");
-            Console.WriteLine($"Statistic:\nNickname: {nickname}\nAge: {age}");
-            Console.WriteLine("========================================");
-            bool isChoise = false;
-            int choise = 0;
-            while (!isChoise)
+            while (true)
             {
-                Console.WriteLine("Press: 1 to play\nPress: 2 to exit the game");
-                var input = Console.ReadLine();
-                bool isNumber = int.TryParse(input, out choise);
-                if (!isNumber || (choise != 1 && choise != 2))
+                Console.WriteLine("========================================");
+                Console.WriteLine($"Statistic:\nNickname: {nickname}\nAge: {age}\nNumber of wins: {playerTotalWins}");
+                Console.WriteLine("========================================");
+                var isChoise = false;
+                var choise = 0;
+                while (!isChoise)
                 {
-                    Console.WriteLine("Invalid input. Try again.");
-                    continue;
+                    Console.WriteLine("Press: 1 to play\nPress: 2 to exit the game");
+                    var input = Console.ReadLine();
+                    var isNumber = int.TryParse(input, out choise);
+                    if (!isNumber || (choise != 1 && choise != 2))
+                    {
+                        Console.WriteLine("Invalid input. Try again.");
+                        continue;
+                    }
+                    isChoise = true;
+                    if (choise == 1)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Goodbye, {nickname}!");
+                        return;
+                    }
                 }
-                isChoise = true;
-                if (choise == 1)
+                Random random = new Random();
+                int[] options = { 1, 2, 3 };
+                int playerWin = 0, computerWin = 0, rounds = 0;
+                while (rounds < 3 && playerWin < 2 && computerWin < 2)
                 {
-                    
+                    int playerChoice = 0;
+                    bool isValidChoice = false;
+                    while (!isValidChoice)
+                    {
+                        Console.WriteLine("Select the type of weapon:\n1.Stone 2.Scissors 3.Paper");
+                        string input = Console.ReadLine();
+                        isValidChoice = int.TryParse(input, out playerChoice) && playerChoice >= 1 && playerChoice <= 3;
+                        if (!isValidChoice) Console.WriteLine("Press 1, 2 or 3!");
+                    }
+                    Console.WriteLine($"You chose: {playerChoice}");
+                    int playerChoise = playerChoice;
+                    int computerChoice = options[random.Next(0, 3)];
+                    string computerChoiceText = computerChoice == 1 ? "Stone" : computerChoice == 2 ? "Scissors" : "Paper";
+                    Console.WriteLine($"Computer choice: {computerChoice} - {computerChoiceText}");
+                    if (playerChoise == computerChoice)
+                    {
+                        Console.WriteLine("Its Draw");
+                        continue;
+                    }
+                    if ((playerChoise == 1 && computerChoice == 2) ||
+                        (playerChoise == 2 && computerChoice == 3) ||
+                        (playerChoise == 3 && computerChoice == 1))
+                    {
+                        Console.WriteLine($"You win this round!");
+                        playerWin++;
+                    }
+                    else
+                    {
+                        computerWin++;
+                        Console.WriteLine($"Computer win this round!");
+                    }
+                    rounds++;
                 }
-                else
+                if (playerWin >= 2)
                 {
-                    Console.WriteLine($"Goodbye, {nickname}!");
-                    return;
+                    Console.WriteLine("You won the battle!");
+                    playerTotalWins++;
                 }
+                else if (computerWin >= 2) Console.WriteLine("Computer won the battle!");
+                else Console.WriteLine("It was a draw");
             }
-            
-            
+
+
+
+
+
+
         }
     }
 }
